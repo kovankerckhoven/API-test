586ac869e21315a4d34ad3fbf879496b5e407e70
Koen Van Kerckhoven <kovankerckhoven@gmail.com>
2017-12-26 16:11:26 +0100

    Calculating averages for population and exchange rates
    Instead of only using the latest population amount and currency exchange rates, calculates the average over the last 30 days of fetched data.

--- README.md
+++ README.md
@@ -4,25 +4,24 @@ EVENTIGRATE API TEST
 Dependencies: Python 2.7, sqlite3, requests (pip install requests)
 
 1. Retrieves data from the https://restcountries.eu/ and http://fixer.io/
-  a.  (free, without registration)
-    * Name
-    * Calling code
-    * Capital
-    * Population
-    * Currency
-    * Flag --------------> in images/<country.svg>
-    * Exchange rate (EUR vs. AUS, BRA, CHN, GBR, USA)
+  * Name
+  * Calling code
+  * Capital
+  * Population
+  * Currency
+  * Flag --------------> in images/<country.svg>
+  * Exchange rate (EUR vs. AUS, BRA, CHN, GBR, USA)
 
 2. Stores this data in a database (SQLite)
-* Table Countries (id, name, callingCode, capital, currency, flag)
-* Table Country_Has_Population (country_id, date, population)
-* Table Rates (id, base, symbol)
-* Table Rate_Has_Value (rate_id, date, value)
+  * Table Countries (id, name, callingCode, capital, currency, flag)
+  * Table Country_Has_Population (country_id, date, population)
+  * Table Rates (id, base, symbol)
+  * Table Rate_Has_Value (rate_id, date, value)
   
 3. Reports this information as static HTML pages
-* countries.html (Contains general information for the countries)
-* flags.html (Contains large-scale versions of the flags)
-* rates.html (Displays all currency exchange rates in a clean overview)
+  * countries.html (Contains general information for the countries)
+  * flags.html (Contains large-scale versions of the flags)
+  * rates.html (Displays all currency exchange rates in a clean overview)
 
 Examples:
 ![Countries](https://i.imgur.com/WNbUrAO.png)
--- update.py
+++ update.py
@@ -21,13 +21,14 @@ import sys
 reload(sys)
 sys.setdefaultencoding('utf8')
 import sqlite3 as sqlite
-from datetime import date
+from datetime import date, timedelta
 from os import path
 
 db = sqlite.connect('countries.db', isolation_level=None)
 db_cursor = db.cursor()
 
 def updateCountries(countries):
+	global db, db_cursor
 	# countries.html
 	with open("countriesBegin.template", 'r') as beginFile:
 		begin = beginFile.readlines()
@@ -40,10 +41,18 @@ def updateCountries(countries):
 			file.write('<div class="text-muted">Capital: %s</div>\n' % country[3])
 			file.write('</div>\n')
 		file.write('</section><h2>Information & statistics</h2><div class="table-responsive"><table class="table table-striped">\n')
-		file.write('<thead><tr><th>#</th><th>Name</th><th>Calling code</th><th>Capital</th><th>Currency code</th></tr></thead>\n')
+		file.write('<thead><tr><th>#</th><th>Name</th><th>Calling code</th><th>Capital</th><th>Population</th><th>Currency code</th></tr></thead>\n')
 		file.write('<tbody>\n')
 		for country in countries: # [0]: id, [1]: name, [2]: callingCode, [3]: capital, [4]: currency, [5]: flag URL
-			file.write('<tr><td>%s</td><td>%s</td><td>%s</td><td>%s</td><td><a href="rates.html#%s">%s</a></td></tr>' % (country[0], country[1], "+%s" % country[2], country[3], country[4], country[4]))
+			with db:
+				db_cursor.execute("SELECT population FROM Country_Has_Population WHERE country_id='%s' AND date>='%s';" % (country[0], date.today() - timedelta(days=30)))
+				populations = db_cursor.fetchall()
+				sum = 0
+				amount = 0
+				for population in populations: # Because we want the average population over the past 30 days
+					sum += population[0]
+					amount += 1
+			file.write('<tr><td>%s</td><td>%s</td><td>%s</td><td>%s</td><td>%s</td><td><a href="rates.html#%s">%s</a></td></tr>' % (country[0], country[1], "+%s" % country[2], country[3], sum/amount, country[4], country[4]))
 		with open("countriesEnd.template", 'r') as endFile:
 			end = endFile.readlines()
 		file.writelines(end)
@@ -53,19 +62,6 @@ def updateCountries(countries):
 	with open("flagsBegin.template", 'r') as beginFile:
 		begin = beginFile.readlines()
 	with open("flags.html", 'w') as file:
-		file.writelines(begin)
-		for country in countries:
-			file.write('<div class="text-muted text-left">%s</div>\n' % country[1])
-			file.write('<section class="row text-center">\n')
-			file.write('<img id="%s" src="images/%s.svg" width="500" class="img-fluid" alt="Flag of %s">\n' % (country[4], country[4], country[1]))
-			file.write('</section><br />\n')
-		with open("flagsEnd.template", 'r') as endFile:
-			end = endFile.readlines()
-		file.writelines(end)
-		file.close()
-
-def updateRates(rates):
-	with open("rates.html", 'w') as file:
 		file.writelines(begin)
 		for country in countries:
 			file.write('<div class="text-muted text-left">%s</div>\n' % country[1])
@@ -89,14 +85,19 @@ def fetchCountries():
 			file.writelines(begin)
 			for country in countries:
 				file.write('</section><div class="table-responsive"><table class="table table-striped">\n')
-				file.write('<thead><tr><th id="%s" colspan="4">%s</th></tr><tr><th>#</th><th>Exchange rate</th><th>Value</th><th>Date</th></tr></thead>\n' % (country[4], country[4]))
+				file.write('<thead><tr><th id="%s" colspan="4">%s</th></tr><tr><th>#</th><th>Exchange rate</th><th>Value</th></tr></thead>\n' % (country[4], country[4]))
 				file.write('<tbody>\n')
 				db_cursor.execute("SELECT id, symbol FROM Rates WHERE base='%s'" % country[4]) # This will hopefully be multiple BASE/SYMBOL rates
 				symbols = db_cursor.fetchall()
 				for symbol in symbols:
-					db_cursor.execute("SELECT value, date FROM Rate_Has_Value WHERE rate_id='%s';" % symbol[0])
-					tmp = db_cursor.fetchall()[0]
-					file.write('<tr><td>%s</td><td>%s/%s</td><td>%s</td><td>%s</td></tr>' % (symbol[0], country[4], symbol[1], tmp[0], tmp[1]))
+					db_cursor.execute("SELECT value FROM Rate_Has_Value WHERE rate_id='%s' AND date>='%s';" % (symbol[0], date.today() - timedelta(days=30)))
+					rates = db_cursor.fetchall()
+					sum = 0
+					amount = 0
+					for rate in rates: # Because we want the average exchange rate over the past 30 days
+						sum += rate[0]
+						amount += 1
+					file.write('<tr><td>%s</td><td>%s/%s</td><td>%.4f</td></tr>' % (symbol[0], country[4], symbol[1], sum/amount))
 			with open("countriesEnd.template", 'r') as endFile:
 				end = endFile.readlines()
 			file.writelines(end)
